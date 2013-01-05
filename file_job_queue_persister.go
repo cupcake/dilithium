026af1fde9605eb0bb00c54c76f3f5ee889c44ad
Daniel Jackoway <danjdel@gmail.com>
2013-01-05 08:07:20 -0800

    working on doing recovery and log rolling

--- file_job_queue_persister.go
+++ file_job_queue_persister.go
@@ -2,11 +2,18 @@ package dilithium
 
 import (
 	"log"
+	"io"
 	"os"
 	"encoding/binary"
+	"strings"
+	"strconv"
 	)
 
 const recordLengthBytes = 4 // Use 32-bit numbers for writing the length in the journal.
+// journal files are named journal-X where X is an integer
+const journalFilenamePrefix = "journal-"
+// TODO this should be set through some config
+const journalDirpath = "journals/"
 
 // Provides a persistent (journaled) job queue.
 // Its goal is that each job in the queue is seen by one reader once.
@@ -20,19 +27,17 @@ const recordLengthBytes = 4 // Use 32-bit numbers for writing the length in the 
 
 // It is not threadsafe. The user must provide some form of synchronization on top.
 // The recommendation is to have one goroutine that owns it and has jobs passed in
-// and out via channels. See other files.
+// and out via channels. JobQueue does this..
 
 type FileJobQueuePersister struct {
-	filename string // Name of the file that backs the queue
+	name string // Name of the file that backs the queue
 	head uint64 // id of the front of the front of the queue (last written)
-	tail uint64 // id of the back of the queue (next to read)
+	tail uint64 // id of the back of the queue (lowest id that has not been Done-ed)
 	// window of outstanding elements. window[i] accounts for the element of id
 	// (i + tail). true means that it has been doneted.
 	window []bool
 	readFile *os.File // File handle that we use to read queued jobs
 	writeFile *os.File // File handle that we use to write new queued jobs
-	headByte int64 // byte offset into journal where head record starts
-	tailByte int64 // byte offset into journal where tail record starts
 }
 
 type Job interface {
@@ -42,52 +47,113 @@ type Job interface {
 	Deserialize([]byte)
 }
 
-func NewFileJobQueuePersister(name string, recovery bool) *FileJobQueuePersister {
+type journalFile struct {
+	basename string
+	lastRecord uint64
+}
+
+func isJournal(name string) bool {
+	return strings.HasPrefix(name, journalFilenamePrefix)
+}
+
+func journalNumber(basename string) int {
+	// TODO error handle
+	num, _ := strconv.Atoi(basename[len(journalFilenamePrefix):])
+	return num
+}
+
+func (q *FileJobQueuePersister) journalDir() string {
+	// TODO at some point should error check to make sure name doesn't have a /
+	return journalDirpath + q.name + "/"
+}
+
+// Opens and reads journal and returns the highest id of any record in the file
+// Also returns an error if there are problems reading the file.
+func (q *FileJobQueuePersister) latestRecord(journal journalFile) (uint64, error) {
+	// TODO, I don't actually have the exact type of Job that we're using.
+	// For now I'm just assuming it's type job (defined in the tests), but we should
+	// either make that not an abstract interface and say that your job will simply
+	// have an id and a slice of bytes (probably best) or have some way to communicate
+	// the exact type to the constructor.
+	max := uint64(0)
+	file, err := os.Open(q.journalDir() + journal.basename)
+	for err != nil {
+		job, e := q.nextInFile(file)
+		if e == nil {
+			if max < job.Id() {
+				max = job.Id()
+			} else {
+				log.Println(job, "followed higher id", max)
+			}
+		} else {
+			err = e
+		}
+	}
+	return max, err
+}
+
+func NewFileJobQueuePersister(name string) *FileJobQueuePersister {
 	q := new(FileJobQueuePersister)
-	if !recovery {
-		// TODO better erorr handling
-		os.Mkdir("journals/", 0777)
-		q.filename = "journals/" + name + ".1"
-		writeFile, err := os.Create(q.filename)
+	q.name = name
+	// TODO better erorr handling
+	os.Mkdir(journalDirpath, 0777)
+	dirname := journalDirpath + name
+	os.Mkdir(dirname, 0777)
+	dirFile, _ := os.Open(dirname)
+	fis, _ := dirFile.Readdir(0)
+	journals := make(map[int]journalFile)
+	for _, fi := range fis {
+		// skip any subdirectories (there shouldn't be any)
+		if !fi.IsDir() {
+			if isJournal(fi.Name()) {
+				journals[journalNumber(fi.Name())] = journalFile{basename: fi.Name()}
+			}
+		}
+	}
+		/*writeFile, err := os.Create(q.filename)
 		if err == nil {
 			// TODO
 			readFile, _ := os.Open(q.filename)
 			q.writeFile = writeFile
 			q.readFile = readFile
-		}
+		 }
 		if err != nil {
 			log.Println("Failure opening journal:", err)
 			return nil
 		} else {
 			return q
-		}
-	} else {
-		panic("Not implemented\n")
-	}
-	return nil
+		}*/
+	return q
 }
 
-func (q *FileJobQueuePersister) Get() Job {
+func (q *FileJobQueuePersister) nextInFile(readFrom *os.File) (*job, error) {
+	j := &job{}
+
 	// read from file
 	lengthBuffer := make([]byte, recordLengthBytes)
 	bytesRead, err := q.readFile.Read(lengthBuffer)
 	if bytesRead < recordLengthBytes || err != nil {
-		// TODO pass back error?
-		return nil
+		if err == io.EOF {
+			// Don't pass back EOF
+			err = nil
+		}
+		return nil, err
 	}
 	recordLength := binary.BigEndian.Uint32(lengthBuffer)
 	jobBuffer := make([]byte, recordLength)
 	bytesRead, err = q.readFile.Read(jobBuffer)
 	if uint32(bytesRead) < recordLength || err != nil {
-		// TODO pass back error?
-		return nil
+		return nil, err
 	}
 
-	j := &job{}
 	j.Deserialize(jobBuffer)
+	return j, nil
+}
 
+func (q *FileJobQueuePersister) Get() Job {
+	j, _ := q.nextInFile(q.readFile)
 
-	// Extend window to track that this job has not been doneted.
+	// Extend window to track that this job has not been done-ed.
 	q.window = append(q.window, false)
 
 	return j
@@ -137,6 +203,10 @@ func firstFalseIndex(slice []bool) int {
 	return len(slice)
 }
 
+func (q *FileJobQueuePersister) updateDurablyWritten() {
+	// TODO write q.tail to some file.
+}
+
 func (q *FileJobQueuePersister) Done(job Job) {
 	ind := q.windowIndex(job)
 	if ind < 0 {
@@ -149,5 +219,7 @@ func (q *FileJobQueuePersister) Done(job Job) {
 		// Slide window forward.
 		q.window = q.window[:newTail]
 		q.tail += uint64(newTail)
+
+		q.updateDurablyWritten()
 	}
 }
--- file_job_queue_persister_test.go
+++ file_job_queue_persister_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestSimple(t *testing.T) {
-	q := NewFileJobQueuePersister("file_job_queue_persister_test", false)
+	q := NewFileJobQueuePersister("file_job_queue_persister_test")
 	if q == nil {
 		t.Error("Failed to create queue")
 	}
 
 	if emptyGet := q.Get(); emptyGet != nil {
-		t.Error("Get when initially empty did not return nil")
+		t.Error("Get when initially empty did not return nil", emptyGet)
 	}
 
 	j1 := &job{}
@@ -21,7 +21,6 @@ func TestSimple(t *testing.T) {
 	j2Serialized[5] = 200
 	j2 := &job{0, j2Serialized}
 	q.Push(j2)
-
 
 	get1 := q.Get().(*job)
 	if !equal(j1, get1) {
@@ -33,7 +32,7 @@ func TestSimple(t *testing.T) {
 	}
 
 	if emptyGet := q.Get(); emptyGet != nil {
-		t.Error("Get after emptied did not return nil")
+		t.Error("Get after emptied did not return nil", emptyGet)
 	}
 
 	q.Done(get2)
